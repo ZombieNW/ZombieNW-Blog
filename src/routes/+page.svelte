e32feeb39f9d723076eda9111ecf1dc6483246fd
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2024-07-16 23:22:28 -0700

    Home Page Design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,17 +22,22 @@
 		return `${month.slice(0, 3)} ${day}, ${year}`;
 	}
 
+	function formatPreview(content) {
+		return content.split(' ').slice(0, 12).join(' ') + (content.split(' ').length > 12 ? '...' : '');
+	}
+
 	onMount(() => {
 		const posts = loadPosts(1, 10);
 	});
 </script>
 
 {#each posts as post}
-	<a href={`/posts/${generateSlug(post.title)}`}>
-		<h1>{post.title}</h1>
-		<h2 class="text-sm">
+	<div class="my-8">
+		<a href={`/posts/${generateSlug(post.title)}`} class="text-2xl font-semibold leading-loose">{post.title}</a>
+		<h2 class="truncate-lines-3 text-light_text_darker dark:text-dark_text_darker flex">{formatPreview(post.content)} <a href="/posts/{generateSlug(post.title)}" class="mx-2 text-light_text dark:text-dark_text underline">Read Post</a></h2>
+		<h2 class="text-sm text-light_text_darker dark:text-dark_text_darker my-4">
 			{formatDate(new Date(post.created_at))}
 			{post.created_at !== post.updated_at ? `• Updated ${formatDate(new Date(post.updated_at))}` : ''}
 		</h2>
-	</a>
+	</div>
 {/each}
