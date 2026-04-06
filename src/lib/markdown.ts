a24b629b18b1e6d05091cf29790356ae5c288c62
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-04-06 13:17:59 -0700

    Syntax highlighter

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,5 +1,24 @@
 import { marked } from 'marked';
+import { createHighlighter } from 'shiki';
+
+// init highligher
+const highlighter = await createHighlighter({
+	themes: ['github-dark', 'github-light'],
+	langs: ['typescript', 'javascript', 'svelte', 'bash', 'css', 'html']
+});
+
+// make highlighter extension
+marked.use({
+	renderer: {
+		code({ text, lang }) {
+			return highlighter.codeToHtml(text, {
+				lang: lang || 'text',
+				theme: 'github-dark'
+			});
+		}
+	}
+});
 
 export async function parseMarkdown(content: string) {
-	return await marked.parse(content);
+	return await marked.parse(content, { async: true });
 }
