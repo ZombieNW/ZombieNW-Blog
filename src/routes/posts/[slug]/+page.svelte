b8ae415393990eb8163342ecb1a091be3312df8d
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-04-09 15:20:18 -0700

    Nicer post page

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -8,14 +8,14 @@
 </svelte:head>
 
 <article class="mx-auto">
-	<header class="mb-8">
-		<h1 class="mb-2 text-4xl font-bold">{data.post.title}</h1>
-		<time class="">
+	<header class="mt-8 mb-4">
+		<h1 class="mb-2 text-5xl font-black">{data.post.title}</h1>
+		<time class="my-1 text-gray-500 dark:text-gray-400">
 			{data.post.createdAt?.toLocaleDateString() ?? 'Unknown'}
 		</time>
 	</header>
 
-	<div class="prose lg:prose-xl dark:prose-invert prose-pre:px-4 prose-pre:py-1">
+	<div class="prose mb-12 lg:prose-xl dark:prose-invert prose-pre:px-4 prose-pre:py-1">
 		{@html data.post.html}
 	</div>
 </article>
