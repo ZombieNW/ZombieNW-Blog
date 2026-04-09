072cd58ab47fd627c7346aa627ac6231e85914a7
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-04-09 15:24:49 -0700

    Change favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href="/logo-small.png" /></svelte:head>
+<svelte:head><link rel="icon" href="/favicon.png" /></svelte:head>
 
 <main class="min-h-screen bg-zinc-50 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-200">
 	<div class="mx-auto w-full max-w-4xl px-4 py-8">
